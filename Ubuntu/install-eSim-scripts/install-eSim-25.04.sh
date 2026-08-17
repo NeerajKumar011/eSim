e25f0510c93bfaad0d7344334c6483765884e022
Neeraj Kumar <kumarneerajwork06@gmail.com>
2026-08-18 00:04:40 +0530

    fix: support NGHDL installation on Ubuntu 25.04

--- Ubuntu/install-eSim-scripts/install-eSim-25.04.sh
+++ Ubuntu/install-eSim-scripts/install-eSim-25.04.sh
@@ -70,7 +70,7 @@ function installNghdl
     # Do not trap on error of any command. Let NGHDL script handle its own errors.
     trap "" ERR
 
-    ./install-nghdl.sh --install       # Install NGHDL
+    bash install-nghdl-scripts/install-nghdl-24.04.sh --install       # Install NGHDL
         
     # Set trap again to error_exit function to exit on errors
     trap error_exit ERR
